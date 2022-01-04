2c217cc6b87fcdc89665c68361d97a0b81e4f795
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-04 15:46:27 +0800

    Delete projects/ngx-prometheus-crud directory

--- projects/ngx-prometheus-crud/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-prometheus-crud
- */
-
-export * from './lib/ngx-prometheus-crud.service';
-export * from './lib/ngx-prometheus-crud.component';
-export * from './lib/ngx-prometheus-crud.module';
